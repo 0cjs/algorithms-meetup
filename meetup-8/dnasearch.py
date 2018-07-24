38a302c9205fa932b1b2c48f52e932f86dd79346
Curt J. Sampson <cjs@cynic.net>
2018-07-24 21:12:01 +0900

    meetup-8/dnasearch: not quite working, and a real mess

--- meetup-8/dnasearch.py
+++ meetup-8/dnasearch.py
@@ -12,8 +12,62 @@ def test(expected, search, sequence):
     assert expected is match(search, sequence)
 
 def match(search, s):
-    return match_trivial(search, s)
+    matchers = []
+    for i in range(len(s)):
+        c = s[i]
+        new_matchers = []
+        for m in matchers:
+            if m.next(c):
+                new_matchers.append(m)
+        if c == search[0]:
+            m = Matcher(search, i)
+            assert m.next(c)            # Awkward; we already did this!
+            new_matchers.append(m)
+        matchers = new_matchers
+    #   We could return a list of positions at which this was found
+    new_matchers = []
+    for m in matchers:
+        if m.complete():
+            new_matchers.append(m)
+    matchers = new_matchers
+    for m in matchers:
+       print(m)
+    return len(new_matchers) > 0
 
+class Matcher:
+    def __init__(self, search, loc=0):
+        self.search = search
+        self.loc = loc
+        self.i = -1
+
+    def next(self, c):
+        " True if c is the next char we're looking for. "
+        self.i += 1
+        if self.complete():
+            return True                 # Final state
+        return c == self.search[self.i]
+
+    def complete(self):
+        return self.i == len(self.search)
+
+    def __str__(self):
+        return 'Matcher loc={} i={} len={} search={}' \
+            .format(self.loc, self.i, len(self.search), self.search)
+
+def test_matcher():
+    assert     Matcher('').next('A')
+    assert     Matcher('A').next('A')
+    assert not Matcher('A').next('B')
+
+    m = Matcher('ABC')
+    assert not m.complete(); assert m.next('A')
+    assert not m.complete(); assert m.next('B')
+    assert not m.complete(); assert m.next('C')
+    assert     m.complete(); assert m.next('D')
+
+    m = Matcher('ABC')
+    assert m.next('A')
+    assert not m.next('C')
 
 ############################################################
 
